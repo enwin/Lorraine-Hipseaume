e318bacf94815189b427e3cb71e1d80b1e7abbce
Thomas Beduneau <thomas@switch-company.com>
2020-05-10 16:22:52 +0200

    random 50 names from a range

--- functions/range.js
+++ functions/range.js
@@ -2,6 +2,64 @@ const data = require('./entries.json');
 
 const paging = 50;
 
+const filterDedupe = (data) => {
+  const deduped = data.reduce((acc, entry) => {
+    const existingIndex = acc.findIndex(({sex, prenom_usuel}) => sex === entry.sex && prenom_usuel === entry.prenom_usuel );
+
+    if( existingIndex >= 0 ){
+      const existingEntry = acc[ existingIndex];
+
+      const annee_naissance = [
+        ...existingEntry.annee_naissance,
+        entry.annee_naissance
+      ];
+
+      annee_naissance.sort();
+
+      acc.splice(existingIndex, 1, {
+        ...existingEntry,
+        nombre: existingEntry.nombre + entry.nombre,
+        annee_naissance,
+      });
+    }
+    else {
+      acc.push( {
+        ...entry,
+        annee_naissance: [entry.annee_naissance]
+      } );
+    }
+    return acc;
+  }, []);
+
+  return Promise.resolve(deduped);
+};
+
+const filterRandom = (data) => {
+  const filtered = [];
+
+  const pick = () => {
+    const entriesCount = data.length - 1;
+    const entry = data.splice(Math.floor(Math.random() * entriesCount), 1);
+
+    return entry;
+  }
+
+  while (filtered.length < 50 && data.length){
+    filtered.push(pick());
+  }
+
+
+  return Promise.resolve(filtered);
+
+};
+
+const filterRange = (data, range) => {
+  const [start, end] = range.split('-');
+
+  return Promise.resolve( data.filter(entry => entry.annee_naissance >= start && entry.annee_naissance <= end ) );
+}
+
+
 exports.handler = async function(event, context, callback) {
   const range = event.queryStringParameters.q;
   const page = event.queryStringParameters.page ? event.queryStringParameters.page-1 : 0;
@@ -13,18 +71,13 @@ exports.handler = async function(event, context, callback) {
     });
   }
 
-  const [start, end] = range.split('-');
-
-  const results = data.filter(entry => entry.annee_naissance >= start && entry.annee_naissance <= end ).sort((a, b) => a.annee_naissance > b.annee_naissance ? 1 : a.annee_naissance < b.annee_naissance ? -1 : 0);
+  const results = await filterRange( data, range )
+    .then(filterDedupe)
+    .then(filterRandom);
 
   const response = {
-    results: results.slice(paging * page, (paging * page) + paging),
-    total: results.length,
-    nextPage: page+2,
-  }
-
-  if( page ){
-    response.lastPage = page;
+    results,
+    total: results.length
   }
 
   callback(null, {
